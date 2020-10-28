69bb73d06d5cf144e7054dfbee86089a831c49dd
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-10-28 11:30:35 -0700

    header completed, sidebar showing options with icons

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar className="header__avatar" alt="Phil Laub" src="" />
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+
+        <input type="text" placeholder="Search Slack" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
